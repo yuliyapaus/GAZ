fa15bb14bc10107bada49186f2996ca3101672c6
vadzimkhatsko <po4taforla2@gmail.com>
2020-05-02 00:22:20 +0300

    vadim's commit3

--- planes/views.py
+++ planes/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
+from django.contrib.auth.forms import User
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib import messages
+from django.core.mail import send_mail
 
 def index(request):
-    return render(request, 'planes/index.html')
+    context = {'pas': BaseUserManager().make_random_password(4)}
+    return render(request, 'planes/index.html',context)
 
 def logout_view(request):
     logout(request)
@@ -30,3 +35,33 @@ def login_view(request,):
     else:
         form = LoginForm()
     return render(request, 'registration/login.html', {'form': form})
+
+def register_view(request):
+    form = None
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Пользователь с таким адресом уже существует')
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Пользователь с таким именем уже существует')
+        else:
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                email = email
+                password = BaseUserManager.make_random_password(4)
+                user = User.objects.create_user(
+                    username,
+                    email,
+                    password
+                )
+                user.save()
+                messages.success(request, 'Регистрация прошла успешно')
+#                send_mail('Hello from GAZ', password, '@gmail.com', [email], fail_silently=False )
+                return redirect('/')
+
+    else:
+        form = RegisterForm()
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
