73daf4905ad53106c3bda4f826d41f3b16350e57
andrey-zubov <55183393+andrey-zubov@users.noreply.github.com>
2020-05-06 20:25:39 +0300

    register of contracts

--- planes/urls.py
+++ planes/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from .views import (
     index,
-    register_view
+    register_view,
+    ContractView,
 )
 
 
 urlpatterns = [
     path('', index),
-    path('register/', register_view, name='register')
+    path('register/', register_view, name='register'),
+    path('contracts/', ContractView.as_view(), name='contracts'),
 
 ]
