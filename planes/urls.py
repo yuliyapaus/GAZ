89bdacba7c879eb66ea7226c64b1ebaea569b772
andrey-zubov <55183393+andrey-zubov@users.noreply.github.com>
2020-05-15 19:16:28 +0300

    Merge from Vadim: User activity journal

--- planes/urls.py
+++ planes/urls.py
@@ -5,8 +5,8 @@ from .views import (
     register_view,
     ContractView,
     fabricate_contract,
+    adding_click_to_UserActivityJournal,
 )
-
 
 
 urlpatterns = [
@@ -15,6 +15,5 @@ urlpatterns = [
     path('contracts/', login_required(ContractView.as_view()), name='contracts'),
     path('contracts/create_contract/', fabricate_contract, name='create_contract'),
     path('contracts/change_contract/<contract_id>', fabricate_contract, name='change_contract'),
-
-
+    path('add_click/', adding_click_to_UserActivityJournal, name='add_click'),
 ]
