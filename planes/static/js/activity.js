90dd2c8e4283449cc96d44ee964bd92b5452b99a
Timur <mr.tiger171@mail.ru>
2020-05-16 16:03:16 +0300

    Merge branch 'master' of https://github.com/vadzimkhatsko/GAZ into vadzimkhatsko-master
    # Conflicts:
    #	planes/views.py

--- planes/static/js/activity.js
+++
@@ -1,7 +0,0 @@
-$("document").ready(function() {
-    $('body').on('keyup mouseup', function () {
-        $.ajax({"url":"/planning/add_click/"
-        })
-        console.log("click")
-    })
-});
