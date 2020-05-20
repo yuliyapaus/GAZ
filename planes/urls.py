992fc3bb7e7b0a1d1ed8d3c09bfe5944d37b0435
andrey-zubov <55183393+andrey-zubov@users.noreply.github.com>
2020-05-20 21:11:55 +0300

    copy-contract is already made - gets id from checkbox, send get-param to contractFabric and creates new contract based on old contract

--- planes/urls.py
+++ planes/urls.py
@@ -9,7 +9,6 @@ from .views import (
     edit_plane,
     add,
     plane,
-    test,
     ContractFabric,
 )
 
@@ -22,11 +21,12 @@ urlpatterns = [
     path('contracts/', login_required(ContractView.as_view()), name='contracts'),
     path('contracts/create_contract/', ContractFabric.as_view(), name='create_contract'),
     path('contracts/change_contract/<contract_id>', ContractFabric.as_view(), name='change_contract'),
+    path('contracts/copy_contract/', ContractFabric.as_view(), name='copy_contract'),
     path('add_click/', adding_click_to_UserActivityJournal, name='add_click'),
     path('<int:finance_cost_id>/curators/', curators, name='curators'),
     path('to_server/', from_js, name='from_js'),
     path('<int:item_id>/edit-plane', edit_plane, name='edit_plane'),
     path('<int:finance_cost_id>/add/', add, name= 'add'),
-
-    path('test/', test)
 ]
+
+
