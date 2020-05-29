c27d3f357cb7e1c82a74be2c7341d232c067de26
fizik003 <kassik0boks@gmail.com>
2020-05-30 02:36:30 +0300

    add year

--- planes/urls.py
+++ planes/urls.py
@@ -17,7 +17,7 @@ app_name = "planes"
 
 
 urlpatterns = [
-    path('', plane, name='plane' ),
+    path('', plane, name='planes' ),
     path('register/', register_view, name='register'),
     path('contracts/', login_required(ContractView.as_view()), name='contracts'),
     path('contracts/create_contract/', ContractFabric.as_view(), name='create_contract'),
@@ -25,10 +25,10 @@ urlpatterns = [
     path('contracts/copy_contract/', ContractFabric.as_view(), name='copy_contract'),
     path('contracts/deleted_contracts/', DeletedContracts.as_view(), name='deleted_contracts'),
     path('add_click/', adding_click_to_UserActivityJournal, name='add_click'),
-    path('<int:finance_cost_id>/curators/', curators, name='curators'),
+    path('<int:year>/<int:finance_cost_id>/curators/', curators, name='curators'),
     path('to_server/', from_js, name='from_js'),
-    path('<int:item_id>/edit-plane', edit_plane, name='edit_plane'),
-    path('<int:finance_cost_id>/add/', add, name= 'add'),
+    path('<int:year>/<int:item_id>/edit-plane', edit_plane, name='edit_plane'),
+    path('<int:year>/<int:finance_cost_id>/add/', add, name= 'add'),
 ]
 
 
