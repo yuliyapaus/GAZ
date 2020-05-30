cb70425f9ba6df25a8cea12d62bd47d3fbdd3207
inity8 <di848588@gmail.com>
2020-05-30 04:12:15 +0300

    add analytics templates

--- analytics/views.py
+++ analytics/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
 
-def get_analytics_page(request):
-    response = {"name": 1, 'email': 2}
+def analytics(request):
+    response = {}
     return render(request, './analytics/analytics.html', response)
 
 
-def get_implementation_plan_page(request):
-    response = {"name": 1, 'email': 2}
+def implementation_plan(request):
+    response = {}
     return render(request, './analytics/implementation_plan.html', response)
+
+
+def deviation_analysis(request):
+    response = {}
+    return render(request, './analytics/deviation_analysis.html', response)
+
+
+def report_RKDZ(request):
+    response = {}
+    return render(request, './analytics/report_RKDZ.html', response)
