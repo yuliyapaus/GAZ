0a3914b4e69da09f180affe93016bb958e0fc937
andrey-zubov <55183393+andrey-zubov@users.noreply.github.com>
2020-06-09 20:07:46 +0300

    fix contract creation - now it work correct

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 from django.contrib.auth.decorators import login_required
 from .views import analytics, implementation_plan, deviation_analysis, report_RKDZ
-# from .analytics import get_analytics_for_all_contracts
+from .analytics import get_analytics_for_all_contracts
 
 app_name = "analytics"
 
 
 urlpatterns = [
     path('', analytics, name='analytics'),
-#    path('implementation_plan/', get_analytics_for_all_contracts, name='implementation_plan'),
+    path('implementation_plan/', get_analytics_for_all_contracts, name='implementation_plan'),
     path('deviation_analysis/', deviation_analysis, name='deviation_analysis'),
     path('report_RKDZ/', report_RKDZ, name='report_RKDZ'),
 
