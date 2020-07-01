7b80e1922dd33caa163999a25afaf086240dbb7d
yuliyapaus <youli_ya@yahoo.com>
2020-07-01 14:30:42 +0300

    Analytics: added third report - RDKZ. In 1,2 report - print, pdf, excel, export

--- analytics/urls.py
+++ analytics/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('implementation_plan/', get_analytics_for_all_contracts, name='implementation_plan'),
     path('deviation_analysis/', get_deviation_analysis, name='deviation_analysis'),
     path('report_RKDZ/', report_RKDZ, name='report_RKDZ'),
-    path('report_RKDZ_table/', report_RKDZ_table, name='report_RKDZ_table'),
+    path('report_RKDZ_table/', report_RKDZ_table, name='report_RKDZ_table')
 ]
