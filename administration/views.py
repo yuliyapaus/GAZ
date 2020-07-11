c6fff0155d802d8e3c9c98788536fc0bf0105f10
berloger <ogir@bk.ru>
2020-07-11 06:32:48 +0300

    add users list output in administration module

--- administration/views.py
+++ administration/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from planes.models import CustomUser
 
 # Create your views here.
 def index(request):
-    context = {}
+    users_list = CustomUser.objects.all()
+    context = {
+        "users_list": users_list,
+    }
     return render(request, 'administration/index.html', context)
